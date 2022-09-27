004183b72f1d1d85bdcffc3c3bbca0e8565c86fa
Omar <omaryusuf88935@gmail.com>
2022-09-27 22:26:56 +0530

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      greeting: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
-
+  
   render() {
 
     return (
