befe707917be53507fc930d4f73d43a5913336e2
omarY23 <111054035+omarY23@users.noreply.github.com>
2022-09-28 17:01:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
+// npm i react-router-dom
+
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { Form } from './Form';
+import Navbar from './Navbar';
+import { About } from './About';
+import {
+  BrowserRouter,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      greeting: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
-
+  
   render() {
 
     return (
-      
+      <BrowserRouter>
+        
+        <Navbar/>
+
       <div className="App">
-        <form action="/" method="POST">
-        <div className="form-group">
-          <div>
-            <div className="form-group">
-              <label forName="Name">Subject</label>
-              <input type="text" className="form-control" name="subject" autocomplete="off" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="body">Email address</label>
-              <input type="text" className="form-control" name="body" autocomplete="off" required />
-              
-            </div>
-            <div className="form-group">
-              <label htmlFor="body">body</label>
-              <input type="text" className="form-control" name="content" autocomplete="off" required />
-            </div>
-            <button type="submit" className="btn btn-primary" name="button">Submit</button>
-            </div>
-            </div>
-            </form>
+      <Switch>
+      <Route path="/" element= {<Form/>}></Route>
+      <Route exact path="/about" element= {<About/>}></Route>
+      </Switch>
       </div>
+
+      </BrowserRouter>
+
+     
       
     );
   }
 }
 
 export default App;
+
+
+//   <form action="/" method="POST">
+// <div className="form-group">
+// <div>
+//   <div className="form-group">
+//     <label forName="Name">Subject</label>
+//     <input type="text" className="form-control" name="subject" autocomplete="off" required />
+//   </div>
+//   <div className="form-group">
+//     <label htmlFor="body">Email address</label>
+//     <input type="text" className="form-control" name="body" autocomplete="off" required />
+    
+//   </div>
+//   <div className="form-group">
+//     <label htmlFor="body">body</label>
+//     <input type="text" className="form-control" name="content" autocomplete="off" required />
+//   </div>
+
+//   <button type="submit" className="btn btn-primary" name="button">Submit</button>
+//   </div>
+//   </div>
+//   </form>
