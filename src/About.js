1e5b18e60e6b2c5c3084d14a5ba27131bb967ae6
omarY23 <111054035+omarY23@users.noreply.github.com>
2022-09-28 17:06:07 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div className='my-3'>This </div>
+    <div className='my-3 mb-3'>We Omar and Mohit currently doing our btech in CSE have developed and designed this web app for demonstration of courier API and how useful it can be  </div>
   )
 }
