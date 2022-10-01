4d463572615578cfdc48ccb6f3dabe7df3184712
Omar <omaryusuf88935@gmail.com>
2022-10-01 19:38:52 +0530

    Adding few more functionalities for homepage

--- src/CardLetters.js
+++ src/CardLetters.js
@@ -8,13 +8,11 @@ export const CardLetters = (props) => {
     <div className="card cardclass1">
               <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <Link to="#" className="btn btn-primary">
+              
+                <img src={props.image} className="card-img-top sizeclass" />
+                {/* <Link to="#" className="btn btn-primary">
                   Go somewhere
-                </Link>
+                </Link> */}
               </div>
             </div>
      
--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import { CardLetters } from "./CardLetters";
+import coverL_img from './Images/coverletter_sample.png'
+import resumeL_img from './Images/resume_sample1.jpg'
+import jobappl_img from './Images/job_application.jpg'
+
+
+
 
 export const Home = () => {
 
@@ -20,6 +26,8 @@ export const Home = () => {
       <div>
       {/* <button className=""image button type="button" data-bs-toggle="collapse" >Get imsge</button> */}
         <div className="d-flex flex-column">
+          <div className="my-3 d-flex justify-content-center align-items-center">
+            <img src="https://media.istockphoto.com/photos/shot-of-two-men-working-on-their-computers-in-a-modern-office-picture-id1319189303?k=20&m=1319189303&s=612x612&w=0&h=IokmOvk7tWi3jnI9Yqnk0z0k1H8BOW0XSIj7yjsZTD0=" className="image"/>
           <div className="container my-3 paddings margins">
             Ogni individuo ha diritto ad un ordine sociale e internazionale nel
             quale i diritti e le libertà enunciati in questa Dichiarazione
@@ -46,33 +54,16 @@ export const Home = () => {
             Dichiarazione possano essere pienamente realizzati. Nulla nella
             presente Dichiarazione può essere interpretato nel senso di
             implicare un diritto di un qualsiasi Stato, gruppo o persona di
-            esercitare un'attività o di compiere un atto mirante alla
-            distruzione di alcuno dei diritti e delle libertà in essa
-            enunciati.Ogni individuo ha diritto ad un ordine sociale e
-            internazionale nel quale i diritti e le libertà enunciati in questa
-            Dichiarazione possano essere pienamente realizzati. Nulla nella
+            esercitare un'att possano essere pienamente realizzati. Nulla nella
             presente Dichiarazione può essere interpretato nel senso di
             implicare un diritto di un qualsiasi Stato, gruppo o persona di
-            esercitare un'attività o di compiere un atto mirante alla
-            distruzione di alcuno dei diritti e delle libertà in essa
-            enunciati.Ogni individuo ha diritto ad un ordine sociale e
-            internazionale nel quale i diritti e le libertà enunciati in questa
-            Dichiarazione possano essere pienamente realizzati. Nulla nella
-            presente Dichiarazione può essere interpretato nel senso di
-            implicare un diritto di un qualsiasi Stato, gruppo o persona di
-            esercitare un'attività o di compiere un atto mirante alla
-            distruzione di alcuno dei diritti e delle libertà in essa
-            enunciati.Ogni individuo ha diritto ad un ordine sociale e
-            internazionale nel quale i diritti e le libertà enunciati in questa
-            Dichiarazione possano essere pienamente realizzati. Nulla nella
-            presente Dichiarazione può essere interpretato nel senso di
-            implicare un diritto di un qualsiasi Stato, gruppo o persona di
-            esercitare un'attività o di compiere
+            esercitare 
           </div>
-          <div className="d-flex flex-row justify-content-center">
-                <CardLetters title='Cover Letter Templates'/>
-                <CardLetters title='Resume Templates'/>
-                <CardLetters title='Appreciation Letter Template'/>
+          </div>
+          <div className="d-flex flex-row justify-content-center margin">
+                <CardLetters title='Cover Letter Template' image= {coverL_img}/>
+                <CardLetters title='Resume Template' image= {resumeL_img}/>
+                <CardLetters title='Job Application Letter Template' image= {jobappl_img}/>
 
           </div>
         </div>
